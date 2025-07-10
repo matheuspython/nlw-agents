e68f3c542124f82771d5b9a5f809c31f0c9e3206
matheus souza <64442354+matheuspython@users.noreply.github.com>
2025-07-10 11:53:07 -0300

    Add files via upload

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,28 @@
-import {fastify} from 'fastify'
-import {
-    serializerCompiler,
-    validatorCompiler,
-    type  ZodTypeProvider
-} from 'fastify-type-provider-zod'
-
 import { fastifyCors } from '@fastify/cors'
+import { fastify } from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
-    origin: 'http://localhost:51733',
+  origin: 'http://localhost:51733',
 })
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
-app.listen({ 
-    port: process.env.PORT ? Number(process.env.PORT) : 3333
- }).then(()=> {
+app.get('/health', () => {
+  return 'ok'
+})
+
+app
+  .listen({
+    port: process.env.PORT ? Number(process.env.PORT) : 3333,
+  })
+  .then(() => {
     console.log('http server runing!')
- })
+  })
