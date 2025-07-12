b8bf8fee0b70dc10d50d31e245231f6eeee11aa2
Matheus Python <ruivorobado@gmail.com>
2025-07-11 22:00:10 -0300

    banco de dados iniciado

--- src/env.ts
+++ src/env.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
+  DATABASE_URLS: z.string().url().startsWith('postgresql://'),
 })
 
 export const env = envSchema.parse(process.env)
-
